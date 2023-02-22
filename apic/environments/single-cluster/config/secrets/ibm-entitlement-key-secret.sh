e6cc159e20f60d8b6cd6711ba40d15bbe153e205
Hien.PhamThe <thehien@telio.vn>
2023-02-22 16:18:00 +0700

    Deliver the IBM entitled registry sealed secret
    Signed-off-by: Hien.PhamThe <thehien@telio.vn>

--- apic/environments/single-cluster/config/secrets/ibm-entitlement-key-secret.sh
+++ apic/environments/single-cluster/config/secrets/ibm-entitlement-key-secret.sh
@@ -16,7 +16,7 @@ oc create secret docker-registry ibm-entitlement-key \
 --docker-username=cp \
 --docker-password=${IBM_ENTITLEMENT_KEY} \
 --docker-server=cp.icr.io \
---dry-run=true -o yaml > delete-ibm-entitlement-key-secret.yaml
+--dry-run=client -o yaml > delete-ibm-entitlement-key-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
 kubeseal -n tools --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-ibm-entitlement-key-secret.yaml > ibm-entitlement-key-secret.yaml
